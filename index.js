00d6efb4c1f7c1bbf15dd22757a4cbcaf7d0007c
Avinash <raavaannn@gmail.com>
2025-05-08 17:01:02 +0530

    Add CORS support and update dependencies

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import 'dotenv/config'
 import express from 'express';
 import paymentRoutes from './route/paymentRoute.js';
 import { connectDB } from './db/db.js';
+import cors from 'cors'
+
+
 
 
 const app = express();
 
 app.use(express.json())
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}))
 
 connectDB()
 
